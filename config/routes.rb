7e4218ecede0e4d48cdca8d8e84e89235f775ff0
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2023-12-22 15:21:10 +0000

    1222

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  root to: "homes#top"
+  get 'homes/about',to: 'homes#about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
